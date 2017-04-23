c0c4b0c7152d7d6a448eaf54b353f8cdb6f9d15c
shiyybua <shiyybua@gmail.com>
2017-04-23 17:29:36 +0800

    groupbykey reading from a file

---
+++ RDD_basic/groupbykey.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from pyspark import SparkContext
+
+path = "/Users/mac/Desktop/data.txt"
+#创建sc
+sc = SparkContext('local', 'app name')
+
+def grouping():
+    file = sc.textFile(path)
+    # 把原文件转换成RDD键值对,key是第一个topicID，value是整个line。
+    content = file.flatMap(lambda line: line.split('\n')).map(lambda content: (content.split('\t')[0], content))
+
+    for topic, c in content.groupByKey().collect():
+        print topic
+        for i, result in enumerate(c):
+            print result
+            if i > 20:
+                break
+
+        print '-'*100
+
+
+
+
+grouping()
